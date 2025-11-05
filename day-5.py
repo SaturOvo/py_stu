9bdc174fe4d584593d36e2e8b91493861021cc83
SaturOvo <3250196824@qq.com>
2025-11-05 22:32:12 +0800

    day5

---
+++ day-5.py
@@ -0,0 +1,36 @@
+#数据结构之 list ,tuple ,str ,set ,dict
+'''
+import random
+
+a1=0
+a2=0
+a3=0
+a4=0
+a5=0
+a6=0
+
+for _ in range(6000):
+    point=random.randrange(1,7)
+    if point==1:
+        a1+=1
+    elif point==2:
+        a2+=1
+    elif point==3:
+        a3+=1
+    elif point==4:
+        a4+=1
+    elif point==5:
+        a5+=1
+    else:
+        a6+=1
+print(f'1出现了{a1}')
+print(f'2出现了{a2}')
+print(f'3出现了{a3}')
+print(f'4出现了{a4}')
+print(f'5出现了{a5}')
+print(f'6出现了{a6}')
+'''
+
+
+
+
