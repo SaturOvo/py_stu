7215fed7e9921c0d16f57ec10a96af197fba6691
SaturOvo <3250196824@qq.com>
2025-11-10 22:49:51 +0800

    Update day-5.py

--- day-5.py
+++ day-5.py
@@ -31,6 +31,57 @@ print(f'5出现了{a5}')
 print(f'6出现了{a6}')
 '''
 
-1
+
+
+item1=list(range(1,11))
+item2=list('hello')
+print(item1)
+print(item2)#['h', 'e', 'l', 'l', 'o']
+#list 创建了列表对象
+# + --->拼接   * --->重复
+#可以使用in 或 not in 来判断元素是否在列表中
+#例
+print(8 in item1)
+print(18 in item1)
+print('a' not in item2)
+print('e' not in item2)
+#切片 (若希望一次性访问多个元素) [start:end:stride]
+print(item1[0:7:3])   #[1, 4, 7]
+print(item1[-1:-7:2]) #注意顺序  []
+print(item1[-7:-1:2]) #[4, 6, 8]
+print(item1[::2])     # [1, 3, 5, 7, 9]
+print(item1[::-1])    #[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(item1[-2:-7:-2])#[9, 7, 5]
+
+#遍历
+#way 1 通过索引运算
+languages=['python','java','c++','kotlin']
+for index in range(len(languages)):
+    print(languages[index])
+
+# way 2 直接循环
+languages=['python','java','c++','kotlin']
+for languages1 in languages:
+    print(languages1)
+
+
+
+import random
+counter=[0]*6
+
+for _ in range(6000):
+    face=random.randrange(1,7)
+    counter[face -1] += 1
+for face in range(1,7):
+    print(f'{face}点输出了{counter[face -1]}次')
+
+
+
+
+
+
+
+
+
 
 
